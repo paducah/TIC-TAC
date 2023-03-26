61eec2d0e62e6bb90dbd3ed54dfffeede34bb992
paducah <awhiting@solstice-mobile.com>
2023-03-26 17:10:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-// Initialize the game state
+// Define variables for the game state
 let currentPlayer = 'X';
 let gameStatus = ['', '', '', '', '', '', '', '', ''];
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
-// Get DOM elements
+// Define winning combinations
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+// Define DOM variables
+const message = document.querySelector('.message');
 const squares = document.querySelectorAll('.square');
-const message = document.getElementById('message');
 
-// Add event listeners to each square
-squares.forEach(square => square.addEventListener('click', handleSquareClick));
+// Define functions
+function currentPlayerTurn() {
+  return `It's ${currentPlayer}'s turn`;
+}
 
-// Handle square clicks
-function handleSquareClick(event) {
-  const square = event.target;
-  const index = Array.from(squares).indexOf(square);
+function winningMessage() {
+  return `${currentPlayer} has won!`;
+}
 
-  // If the square is already filled or the game is over, return
-  if (gameStatus[index] !== '' || !gameStatus.includes('')) {
-    return;
+function drawMessage() {
+  return `Game ended in a draw!`;
+}
+
+function checkForWin() {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (
+      gameStatus[a] !== '' &&
+      gameStatus[a] === gameStatus[b] &&
+      gameStatus[b] === gameStatus[c]
+    ) {
+      return true;
+    }
   }
+  return false;
+}
 
-  // Update the game state
-  gameStatus[index] = currentPlayer;
-  square.textContent = currentPlayer;
-  square.classList.add(`player${currentPlayer}`);
+function handleSquareClick(squareClicked, squareIndex) {
+  gameStatus[squareIndex] = currentPlayer;
+  squareClicked.classList.add(`player${currentPlayer}`);
 
-  // Check if the game is over
   if (checkForWin()) {
     message.textContent = winningMessage();
+    restartGame();
   } else if (!gameStatus.includes('')) {
     message.textContent = drawMessage();
   } else {
@@ -38,50 +60,19 @@ function handleSquareClick(event) {
   }
 }
 
-// Check if any player has won the game
-function checkForWin() {
-  const winConditions = [
-    // Rows
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    // Columns
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    // Diagonals
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
-  for (let i = 0; i < winConditions.length; i++) {
-    const [a, b, c] = winConditions[i];
-    if (gameStatus[a] !== '' && gameStatus[a] === gameStatus[b] && gameStatus[b] === gameStatus[c]) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-// Initialize the game
-function startGame() {
+function restartGame() {
+  currentPlayer = 'X';
+  gameStatus = ['', '', '', '', '', '', '', '', ''];
+  squares.forEach((square) => {
+    square.classList.remove('playerX', 'playerO');
+  });
   message.textContent = currentPlayerTurn();
 }
 
-// Restart the game
-function restartGame() {
-  gameStatus = ['', '', '', '', '', '', '', '', ''];
-  currentPlayer = 'X';
-  squares.forEach(square => {
-    square.textContent = '';
-    square.classList.remove('playerX', 'playerO');
-  });
-  startGame();
-}
+// Add event listeners to the game board squares
+squares.forEach((square, index) => {
+  square.addEventListener('click', () => handleSquareClick(square, index));
+});
 
-// Add event listener to the "Restart" button
-document.getElementById('restartButton').addEventListener('click', restartGame);
-
-// Start the game
-startGame();
+// Initialize the game
+message.textContent = currentPlayerTurn();
