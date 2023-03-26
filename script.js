3b0781a1e8998f6a325d41b35a31505476874280
paducah <awhiting@solstice-mobile.com>
2023-03-26 16:31:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+let currentPlayer = 'X';
+let gameOver = false;
+
+function printBoard() {
+  for (let i = 0; i < 9; i++) {
+    document.getElementById(i.toString()).innerText = board[i];
+  }
+}
+
+function checkWin() {
+  // check rows
+  for (let i = 0; i < 9; i += 3) {
+    if (board[i] === board[i+1] && board[i+1] === board[i+2] && board[i] !== ' ') {
+      return true;
+    }
+  }
+  // check columns
+  for (let i = 0; i < 3; i++) {
+    if (board[i] === board[i+3] && board[i+3] === board[i+6] && board[i] !== ' ') {
+      return true;
+    }
+  }
+  // check diagonals
+  if ((board[0] === board[4] && board[4] === board[8]) || (board[2] === board[4] && board[4] === board[6])) {
+    return true;
+  }
+  // no winner
+  return false;
+}
+
+function playTurn(square) {
+  const id = parseInt(square.id);
+  if (board[id] === ' ' && !gameOver) {
+    board[id] = currentPlayer;
+    printBoard();
+    if (checkWin()) {
+      gameOver = true;
+      document.getElementById('message').innerText = `Player ${currentPlayer} wins!`;
+    } else if (!board.includes(' ')) {
+     
+
+     
